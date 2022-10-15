6ce755ffdeae3a10a5364c0846a600a48fc37cbe
Connor Sutherland-Stewart <connor.b.stewart.36@gmail.com>
2022-10-15 17:23:50 -0400

    Inventory Scroll

--- Project Files/Scripts/UI/MainMenu.gd
+++ Project Files/Scripts/UI/MainMenu.gd
@@ -5,7 +5,9 @@ signal Memo
 signal KeyItems
 signal Options
 
+onready var _itemRowNode = $MarginContainer/VBoxContainer/ItemRow
 
+var item_hover_over = 3
 
 
 func _on_Map_pressed():
@@ -25,4 +27,21 @@ func _on_KeyItems_pressed():
 
 func _on_Options_pressed():
 	emit_signal("Options")
+	pass # Replace with function body.
+
+func update_Item_Scroll_position(value)->void:
+	item_hover_over += value
+	if value > 0:
+		_itemRowNode.move_child(_itemRowNode.get_child(0),_itemRowNode.get_child_count()-1)
+	if value < 0:
+		_itemRowNode.move_child(_itemRowNode.get_child(_itemRowNode.get_child_count()-1),0)
+	pass
+
+func _on_Item_Scroll_Previous_pressed():
+	get_parent().click_success()
+	update_Item_Scroll_position(-1)
+	
+func _on_Item_Scroll_Next_pressed():
+	get_parent().click_success()
+	update_Item_Scroll_position(1)
 	pass # Replace with function body.
