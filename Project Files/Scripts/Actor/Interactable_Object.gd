f022d3426eee5a6774268d6fc2fa6b3e17251973
Connor Sutherland-Stewart <connor.b.stewart.36@gmail.com>
2022-10-25 19:36:54 -0400

    added text files that include choices

--- Project Files/Scripts/Actor/Interactable_Object.gd
+++ Project Files/Scripts/Actor/Interactable_Object.gd
@@ -1,7 +1,15 @@
 extends InteractableObjects
+tool
 
+export (Vector2) var scale_of_interactable_box = Vector2(1,1)
+
+ 
 func _ready():
 	self.texture = spriteToLoad
 	change_collision_and_mask($Area2D,floor_placement+9,true)
 	set_hitboxes_state(hitboxes_enabled)
+	$Area2D/CollisionShape2D.scale = scale_of_interactable_box
 
+func _process(delta):
+	if Engine.editor_hint:
+		$Area2D/CollisionShape2D.scale = scale_of_interactable_box
